154b49efd26e048d321c823ce46973df36d46e3c
micho132 <michelbouchahine132@gmail.com>
2024-07-04 06:48:37 +0300

    add required input validation to image model

--- LabSession1/Models/Image.cs
+++ LabSession1/Models/Image.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LabSession1.Models;
 
 public class Image
 {
     public string name { get; set; }
+    [Required]
     public IFormFile file { get; set; }
 }
